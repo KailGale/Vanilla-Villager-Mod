18e9e3bb1438caab23e0c82a3f552bb6d88dc569
micha <micha@10.0.0.9>
2018-06-06 21:28:16 -0700

    master setup

---
+++ src/main/java/com/kailgale/vanillavillagerspp/Main.java
@@ -0,0 +1,13 @@
+package com.kailgale.vanillavillagerspp;
+
+import com.kailgale.vanillavillagerspp.util.Reference;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION)
+
+public class Main 
+{
+	
+	
+
+}
---
+++ src/main/java/com/kailgale/vanillavillagerspp/util/Reference.java
@@ -0,0 +1,13 @@
+package com.kailgale.vanillavillagerspp.util;
+
+public class Reference {
+	
+	public static final String MOD_ID = "kgvvpp";
+	public static final String NAME = "";
+	public static final String VERSION = "";
+	public static final String ACCEPTED_VERSIONS = "{1.12.2}";
+	public static final String CLIENT_PROXY_CLASS = "";
+	public static final String COMMON_PROXY_CLASS = "";
+	
+
+}
