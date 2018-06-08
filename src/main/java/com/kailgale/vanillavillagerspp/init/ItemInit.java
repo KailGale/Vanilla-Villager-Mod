9248aeaeb4df9bbf6ac6180b73e3ff18eb412325
micha <micha@10.0.0.9>
2018-06-07 20:44:40 -0700

    fixed item renderer

--- src/main/java/com/kailgale/vanillavillagerspp/init/ItemInit.java
+++ src/main/java/com/kailgale/vanillavillagerspp/init/ItemInit.java
@@ -11,6 +11,6 @@ public class ItemInit {
 	
 	public static final List<Item> ITEMS = new ArrayList<Item>();
 	
-	public static final Item TEST_STRING = new ItemBase("string_test");
+	public static final Item INGOT_TEST = new ItemBase("ingot_test");
 
 }
--- src/main/java/com/kailgale/vanillavillagerspp/proxy/ClientProxy.java
+++ src/main/java/com/kailgale/vanillavillagerspp/proxy/ClientProxy.java
@@ -10,6 +10,7 @@ public class ClientProxy extends CommonProxy{
 	public void registerItemRenderer(Item item, int meta, String id) {
 		
 		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
+		
 	}
 
 }
