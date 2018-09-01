56b318b85630a445e5aa2a82cae8c4ddb1cde77a
KailGale <40012089+KailGale@users.noreply.github.com>
2018-08-31 17:32:21 -0700

    Revert "Blocks and Items"
    This reverts commit a28d78d9d416e9898148731ba4ed32591df89ee5.

--- src/main/java/com/kailgale/vanillavillagerspp/init/BlockInit.java
+++ src/main/java/com/kailgale/vanillavillagerspp/init/BlockInit.java
@@ -1,16 +1,5 @@
 package com.kailgale.vanillavillagerspp.init;
 
-import java.util.ArrayList;
-import java.util.List;
+public class BlockInit {
 
-import com.kailgale.vanillavillagerspp.objects.blocks.BlockBase;
-
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
-
-public class BlockInit 
-{
-	public static final List<Block> BLOCKS = new ArrayList<Block>();
-	
-	public static final Block BLOCK_TEST = new BlockBase("block_test", Material.IRON);
 }
